ec1836060b4bd682a1d166c764feb4300ed3e72a
param999 <pgpro111@gmail.com>
2020-01-30 17:57:27 +0530

    Updates

---
+++ precache-manifest.d70af065a59873913a21c8ddbca4c652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ed86747211592181a104622919992a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b42bcb940638074e22a",
+    "url": "/monsters-rolodex/static/css/main.19b8e60f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b42bcb940638074e22a",
+    "url": "/monsters-rolodex/static/js/main.36e8e070.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
